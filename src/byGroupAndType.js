faa594fcb8d66a1cf89a536d86d6fcc0c4152828
Aaron Lademann <aaron.lademann@workiva.com>
2015-01-16 11:00:47 -0700

    Group functions and mixins into a 'helpers' group

--- src/byGroupAndType.js
+++ src/byGroupAndType.js
@@ -6,6 +6,8 @@ module.exports = function (data){
   var byGroupAndType = {};
 
   eachItem(data, function (item, type){
+    var niceType = type == 'variable' ? 'variable' : 'helper';
+
     var group = item.group[0][0];
 
     if (byGroupAndType[group] === undefined) {
@@ -17,6 +19,20 @@ module.exports = function (data){
     }
 
     byGroupAndType[group][type].push(item);
+
+    //
+    // Create a "supergroup" combining mixin and functions
+    //
+    if (niceType == 'helper') {
+
+      if (!Array.isArray(byGroupAndType[group][niceType])){
+        byGroupAndType[group][niceType] = [];
+      }
+
+      byGroupAndType[group][niceType].push(item);
+
+    }
+
   });
 
   return byGroupAndType;
